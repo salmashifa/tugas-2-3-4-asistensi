c4088d349d30190f4138eefb25daab2543f9b57a
nbilaalya2406-eng <nbilaalya2406@gmail.com>
2026-06-06 10:37:00 +0700

    halaman home

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Menghubungkan halaman utama (/) ke HomeController
+Route::get('/', [HomeController::class, 'index'])->name('home');
